0b5a92a0eb487d5122accbc72eb1d7bd6fd7f9fd
masachejair-rgb <masachejair@gmail.com>
2026-07-28 15:38:22 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
+// Variable de versión: ¡Cambia este número (v2, v3...) cada vez que actualices el HTML!
+const CACHE_NAME = 'genutrack-v2'; 
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('genutrack-store').then((cache) => cache.addAll([
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([
+      '/',
       '/index.html',
       '/manifest.json'
     ]))
+  );
+});
+
+// Este evento es NUEVO: Se encarga de borrar la caché vieja del celular
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Borrando caché antigua:', key);
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
 
